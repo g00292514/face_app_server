d99c62ea0198166f0c6c1038632ddbe92b8424e3
g00292514 <31918216+g00292514@users.noreply.github.com>
2018-07-02 01:55:34 +0100

    adding db

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const pgres = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
+		host: 'postgresql-cylindrical-99027',
 		user: 'postgres',
 		password: 'admin',
 		database: 'face-app'
