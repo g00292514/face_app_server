ae09e5e8a60591ad178108d78f59bbbb67010388
g00292514 <31918216+g00292514@users.noreply.github.com>
2018-07-02 01:17:20 +0100

    upload version

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/profile/:id', (req , res) => { profile.handleProfileGet(req, res, pgre
 app.put('/image', (req, res) => { image.handleImage(req, res, pgres)});
 app.post('/imageurl', (req, res) => { image.handleAPICall(req, res)});
 
-app.listen(3000, () => {
-	console.log('app is running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${PORT}`)
 })
